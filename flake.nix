8d1a7a7ad78f17609f48f5c8095d78e6169a9cbc
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-09 01:37:42 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Virgutils, multiple utils used in Zhaith Izaliel's system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system  = "x86_64-linux";
+  in
+  {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
+
