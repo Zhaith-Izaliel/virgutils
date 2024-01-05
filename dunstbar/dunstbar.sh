3ffb54689ed48cca095d663945393ff17663ed0d
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-05 17:06:15 +0100

    chore: bump version

--- dunstbar/dunstbar.sh
+++ dunstbar/dunstbar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="1.4.4"
+VERSION="1.4.5"
 
 # Print colors
 RED="\033[0;31m"
--- wlogout-blur/wlogout-blur.sh
+++ wlogout-blur/wlogout-blur.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="1.4.4"
+VERSION="1.4.5"
 WLOGOUT_BLUR_IMAGE_LOCATION="/tmp/wlogout-blur.png"
 
 usage() {
