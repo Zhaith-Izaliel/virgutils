df88b9dc7cf28eb0186e2ea45940d0315c4c010e
Zhaith Izaliel <zhaith.izaliel@ethereal-edelweiss.cloud>
2026-05-31 13:16:35 +0200

    feat(flake): update flake

--- flake.nix
+++ flake.nix
@@ -2,43 +2,51 @@
   description = "Virgutils, multiple utils used in Zhaith Izaliel's system";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
-  outputs = inputs @ {flake-parts, ...}: let
-    version = "1.19.0";
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
-      imports = [
-        flake-parts.flakeModules.easyOverlay
-      ];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    let
+      version = "1.19.0";
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
+      {
+        imports = [
+          flake-parts.flakeModules.easyOverlay
+        ];
 
-      systems = ["x86_64-linux" "aarch64-linux"];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
 
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: {
-        overlayAttrs = config.packages;
+        perSystem =
+          {
+            config,
+            pkgs,
+            ...
+          }:
+          {
+            overlayAttrs = config.packages;
 
-        devShells = {
-          # nix develop
-          default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              brightnessctl
-              libnotify
-              coreutils
-              node2nix
-            ];
+            devShells = {
+              # nix develop
+              default = pkgs.mkShell {
+                nativeBuildInputs = with pkgs; [
+                  brightnessctl
+                  libnotify
+                  coreutils
+                ];
+              };
+            };
+
+            packages = {
+              dim-on-lock = pkgs.callPackage ./dim-on-lock { inherit version; };
+              power-management = pkgs.callPackage ./power-management { inherit version; };
+            };
           };
-        };
-
-        packages = {
-          dim-on-lock = pkgs.callPackage ./dim-on-lock {inherit version;};
-          nix-npm-install = pkgs.callPackage ./nix-npm-install {inherit version;};
-          power-management = pkgs.callPackage ./power-management {inherit version;};
-        };
-      };
-    });
+      }
+    );
 }
